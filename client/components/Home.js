0af9f5a88bc0e82b2df4d414f7e8e7aa6c6686d2
Fayan1103 <enida.vezelaj@hotmail.it>
2019-10-09 19:56:36 -0400

    changed about me text

--- client/components/Home.js
+++ client/components/Home.js
@@ -9,7 +9,9 @@ class Home extends React.Component {
     <div id="text">
     <h1>Enida Vezelaj</h1>
       <h3>About me</h3>
-      <p>I am a New York based software developer. I have been learning how to code and solve algorithms on my own and decided to persue this carreer. I attended the Grace Hopper bootcamp at Fullsdtack Academy of code, where I got my most recent education.  I am experienced working with Java Script, NERD Stack (Node.js, Express, React, Databases). I love coming up with solutions and learning new ways to approach a problem, this is why I decided to do software development.
+      <p>I am a New York based software developer. I have been learning how to code and solve algorithms on my own for the past couple of years and finally decided to persue this carreer. I gradauted from Grace Hopper at Fullsdtack Academy of code and learned to work as a fullstack developer.
+      I am experienced with Java Script, NERD Stack (Node.js, Express, React, Databases) for the backend and I am proficient with React for the front end.
+      I like to build strong long lasting applications that will make people s personal or working life easier.
       </p>
       </div>
       </div>
