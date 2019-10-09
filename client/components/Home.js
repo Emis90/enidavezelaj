386c7ea133a5ba25c4d2d215d145ed8bc1b8dbf1
Fayan1103 <enida.vezelaj@hotmail.it>
2019-10-08 21:17:39 -0400

    minor change on home page

--- client/components/Home.js
+++ client/components/Home.js
@@ -20,41 +20,17 @@ class Home extends React.Component {
      return(
     <div className='about' >
     <div className='me'>
-
-      classNames="window"
-      in={this.state.view}
-      timeout={350}
-      unmountOnExit
-      appear
-      >
     <h1>Enida Vezelaj</h1>
-
-      classNames="window"
-      in={this.state.view}
-      timeout={600}
-      unmountOnExit
-      appear
-      >
       <div>
       <h3>About me</h3>
       <img src='/images/heart.png' width='40' height='40'/>
       </div>
-
-       classNames="window"
-       in={this.state.view}
-       timeout={1000}
-       unmountOnExit
-       appear
-       >
     <div className='slide'>
     <div id="text">
       <p>I am a New York based software developer. I have been learning how to code and solve algorithms on my own and decided to persue this carreer. I attended the Grace Hopper bootcamp at Fullsdtack Academy of code, where I got my most recent education.  I am experienced working with Java Script, NERD Stack (Node.js, Express, React, Databases). I love coming up with solutions and learning new ways to approach a problem, this is why I decided to do software development.
       </p>
-
-
       </div>
       <div id="btn">
-
       </div>
       </div>
       </div>
