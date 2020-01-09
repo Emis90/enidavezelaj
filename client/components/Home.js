d524489a52619670d85fc8280bcaf7331a3f8326
Fayan1103 <enida.vezelaj@hotmail.it>
2020-01-09 02:07:51 -0500

    fixed typo

--- client/components/Home.js
+++ client/components/Home.js
@@ -11,9 +11,9 @@ class Home extends React.PureComponent {
       <h1>Enida Vezelaj</h1>
       <h3>Software engineer | NY</h3>
     </div>
-      <p>I am a New York based software developer. I have been learning how to code and solve algorithms on my own for the past couple of years and finally decided to persue this carreer. I gradauted from Grace Hopper at Fullsdtack Academy of code and learned to work as a fullstack developer.
+      <p>I am a New York based software developer. I have been learning how to code and solve algorithms on my own for the past couple of years and finally decided to persue this carreer. I gradauted from Grace Hopper at Fullstack Academy of code and learned to work as a fullstack developer.
       I am experienced with Java Script, React, NERD Stack (Node.js, Express, React, Databases) and I know how to teach myself new technologies.
-      I am passionate about what I do and I am motivated by new challanges. Feel free to checkout some of my projects or contact me with any questions.
+      I am passionate about what I do and I am motivated by new challanges.
       </p>
       </div>
       </div>
