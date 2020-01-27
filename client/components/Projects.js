fd11f9b9fafca4bb7cfa64c0cad522f895b37202
Fayan1103 <enida.vezelaj@hotmail.it>
2020-01-27 14:06:10 -0500

    final spacing css, responsive design

--- client/components/Projects.js
+++ client/components/Projects.js
@@ -13,12 +13,17 @@ class Projects extends React.PureComponent {
               <div className="description">
               <h3>Real Estate Website</h3>
               <p>Fullstck developemt to mimic a regular real estate website where users can find a cool museum</p>
+              <div id='tech-used'>
+              <p className='tech-used'>Node.js</p>
+              <p className='tech-used'>PostgreSQL</p>
+              <p className='tech-used'>React</p>
+              </div>
               </div>
             </div>
             <div className="back">
               <p className='contribution'>Made a one time call to a public museum api, seeded my postgres database, added new info like rating, description, price and ghost presence. I set up the routes to get/change status of sold or available museums and used Passport.js for user authentication.
               I used React on the frontend and purposly kept all the components and map on the same page to let the client se changes in real time while searching based on preference.</p>
-              <p>Node.js, Sequelize, Express, React</p>
+
               <a href="https://museumsofnyc.herokuapp.com">Get your own museum!</a>
               <div>
                 <a href="https://github.com/Emis90/castelli">
@@ -35,11 +40,17 @@ class Projects extends React.PureComponent {
               <div className="description">
               <h3>Grace in space</h3>
               <p>Mobile VR game where the player collects items floating away in space.</p>
+              <div id='tech-used'>
+              <p className='tech-used'>Viro</p>
+              <p className='tech-used'>React Native</p>
+              <p className='tech-used'>Redux</p>
+              <p className='tech-used'>Firebase/Firestore</p>
               </div>
+              </div>
+
             </div>
             <div className="back">
               <p className='contribution'>Set up initial stack, buit the navigation from dynamic VR component and static React component. I set up the redux store as state management system to allow better data passing throghout the app and fix the memory leakage. Lastly I implemented user authentication with Firebase and Firestore for database to store user points.</p>
-              <p>Viro, ReactNative, Redux, Firebase, firestore</p>
               <div>
                 <a href="https://github.com/graceinspace/graceInSpace">
                 GitHub
@@ -55,11 +66,15 @@ class Projects extends React.PureComponent {
               <div className="description">
               <h3>Gelato finder</h3>
               <p>Mobile application for finding the best sweet spots in the city</p>
+              <div id='tech-used'>
+              <p className='tech-used'>React Native</p>
+              <p className='tech-used'>Firebase</p>
+              </div>
               </div>
               </div>
               <div className="back">
               <p className='contribution'>This was my first React Native project where I used api a public for fetching name and location coordinates of sweet spots in NYC to display on a google map. I used a tab type navigation and I implemented Firebase user authentication and Firestore badabase for saving notes.</p>
-              <p>React-Native, Firebase</p>
+
               <div>
                 <a href="https://github.com/Emis90/GelatoFinder">
                 GitHub
@@ -76,11 +91,15 @@ class Projects extends React.PureComponent {
               <div className="description">
               <h3>Cupcake store</h3>
               <p>E-commerce website for out of this world cupcakes!</p>
+              <div id='tech-used'>
+              <p className='tech-used'>React Native</p>
+              <p className='tech-used'>Firebase</p>
+              <p className='tech-used'>Firestore</p>
+              </div>
               </div>
               </div>
             <div className="back">
               <p className='contribution'>I planned the schema for database tables and set up relationships between users and orders. I seeded the database with data and set up routes. I wrote routes for a non-logged user shopping experience and lastly used Flexbox for styling.</p>
-              <p>Node.js React, Redux, Flexbox</p>
               <div>
               <a href="https://fantasticsweetcupcakes.herokuapp.com">Buy some cupcakes!</a>
               </div>
@@ -99,11 +118,14 @@ class Projects extends React.PureComponent {
               <div className="description">
               <h3>npm module</h3>
               <p>Matching alorithm for two sets of elements base off of the stable marriage alorithm</p>
+              <div id='tech-used'>
+              <p className='tech-used'>Node.js</p>
+              </div>
               </div>
               </div>
               <div className="back">
                 <p className='contribution'>Wrote and published to npm a matching algorith that will pair one group of people to the next based on each others preferene on each other.</p>
-                <p>Node.js, npm</p>
+
                 <div>
                 <a href="https://www.npmjs.com/package/matchingev-">
                 GitHub
--- public/bundle.js
+++ public/bundle.js
@@ -417,11 +417,19 @@ function (_React$PureComponent) {
         src: "/images/museum.jpg"
       }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "description"
-      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", null, "Real Estate Website"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "Fullstck developemt to mimic a regular real estate website where users can find a cool museum"))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", null, "Real Estate Website"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "Fullstck developemt to mimic a regular real estate website where users can find a cool museum"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        id: "tech-used"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+        className: "tech-used"
+      }, "Node.js"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+        className: "tech-used"
+      }, "PostgreSQL"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+        className: "tech-used"
+      }, "React")))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "back"
       }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
         className: "contribution"
-      }, "Made a one time call to a public museum api, seeded my postgres database, added new info like rating, description, price and ghost presence. I set up the routes to get/change status of sold or available museums and used Passport.js for user authentication. I used React on the frontend and purposly kept all the components and map on the same page to let the client se changes in real time while searching based on preference."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "Node.js, Sequelize, Express, React"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+      }, "Made a one time call to a public museum api, seeded my postgres database, added new info like rating, description, price and ghost presence. I set up the routes to get/change status of sold or available museums and used Passport.js for user authentication. I used React on the frontend and purposly kept all the components and map on the same page to let the client se changes in real time while searching based on preference."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
         href: "https://museumsofnyc.herokuapp.com"
       }, "Get your own museum!"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
         href: "https://github.com/Emis90/castelli"
@@ -439,11 +447,21 @@ function (_React$PureComponent) {
         src: "/images/ship.png"
       }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "description"
-      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", null, "Grace in space"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "Mobile VR game where the player collects items floating away in space."))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", null, "Grace in space"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "Mobile VR game where the player collects items floating away in space."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        id: "tech-used"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+        className: "tech-used"
+      }, "Viro"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+        className: "tech-used"
+      }, "React Native"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+        className: "tech-used"
+      }, "Redux"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+        className: "tech-used"
+      }, "Firebase/Firestore")))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "back"
       }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
         className: "contribution"
-      }, "Set up initial stack, buit the navigation from dynamic VR component and static React component. I set up the redux store as state management system to allow better data passing throghout the app and fix the memory leakage. Lastly I implemented user authentication with Firebase and Firestore for database to store user points."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "Viro, ReactNative, Redux, Firebase, firestore"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+      }, "Set up initial stack, buit the navigation from dynamic VR component and static React component. I set up the redux store as state management system to allow better data passing throghout the app and fix the memory leakage. Lastly I implemented user authentication with Firebase and Firestore for database to store user points."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
         href: "https://github.com/graceinspace/graceInSpace"
       }, "GitHub"))))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "proj",
@@ -459,11 +477,17 @@ function (_React$PureComponent) {
         src: "/images/gelato.png"
       }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "description"
-      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", null, "Gelato finder"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "Mobile application for finding the best sweet spots in the city"))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", null, "Gelato finder"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "Mobile application for finding the best sweet spots in the city"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        id: "tech-used"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+        className: "tech-used"
+      }, "React Native"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+        className: "tech-used"
+      }, "Firebase")))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "back"
       }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
         className: "contribution"
-      }, "This was my first React Native project where I used api a public for fetching name and location coordinates of sweet spots in NYC to display on a google map. I used a tab type navigation and I implemented Firebase user authentication and Firestore badabase for saving notes."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "React-Native, Firebase"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+      }, "This was my first React Native project where I used api a public for fetching name and location coordinates of sweet spots in NYC to display on a google map. I used a tab type navigation and I implemented Firebase user authentication and Firestore badabase for saving notes."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
         href: "https://github.com/Emis90/GelatoFinder"
       }, "GitHub"))))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "proj",
@@ -479,11 +503,19 @@ function (_React$PureComponent) {
         src: "/images/cupcake.png"
       }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "description"
-      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", null, "Cupcake store"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "E-commerce website for out of this world cupcakes!"))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", null, "Cupcake store"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "E-commerce website for out of this world cupcakes!"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        id: "tech-used"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+        className: "tech-used"
+      }, "React Native"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+        className: "tech-used"
+      }, "Firebase"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+        className: "tech-used"
+      }, "Firestore")))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "back"
       }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
         className: "contribution"
-      }, "I planned the schema for database tables and set up relationships between users and orders. I seeded the database with data and set up routes. I wrote routes for a non-logged user shopping experience and lastly used Flexbox for styling."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "Node.js React, Redux, Flexbox"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+      }, "I planned the schema for database tables and set up relationships between users and orders. I seeded the database with data and set up routes. I wrote routes for a non-logged user shopping experience and lastly used Flexbox for styling."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
         href: "https://fantasticsweetcupcakes.herokuapp.com"
       }, "Buy some cupcakes!")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
         href: "https://github.com/fantastic-four-grace-shopper/grace-shopper"
@@ -499,11 +531,15 @@ function (_React$PureComponent) {
         src: "/images/npm.png"
       }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "description"
-      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", null, "npm module"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "Matching alorithm for two sets of elements base off of the stable marriage alorithm"))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", null, "npm module"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "Matching alorithm for two sets of elements base off of the stable marriage alorithm"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        id: "tech-used"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+        className: "tech-used"
+      }, "Node.js")))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "back"
       }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
         className: "contribution"
-      }, "Wrote and published to npm a matching algorith that will pair one group of people to the next based on each others preferene on each other."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "Node.js, npm"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+      }, "Wrote and published to npm a matching algorith that will pair one group of people to the next based on each others preferene on each other."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
         href: "https://www.npmjs.com/package/matchingev-"
       }, "GitHub"))))));
     }
@@ -39774,7 +39810,7 @@ function warning(message) {
 /*!***************************************************************!*\
   !*** ./node_modules/react-router-dom/esm/react-router-dom.js ***!
   \***************************************************************/
-/*! exports provided: BrowserRouter, HashRouter, Link, NavLink, MemoryRouter, Prompt, Redirect, Route, Router, StaticRouter, Switch, generatePath, matchPath, withRouter, __RouterContext */
+/*! exports provided: MemoryRouter, Prompt, Redirect, Route, Router, StaticRouter, Switch, generatePath, matchPath, withRouter, __RouterContext, BrowserRouter, HashRouter, Link, NavLink */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
