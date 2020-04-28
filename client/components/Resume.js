52bcc0c8bc44ddf76f7c286af3e21504a2fbc8b7
Fayan1103 <enida.vezelaj@hotmail.it>
2020-04-28 13:22:06 -0400

    updated resume

--- client/components/Resume.js
+++ client/components/Resume.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import React from "react";
+import { motion } from "framer-motion";
 
 class Resume extends React.PureComponent {
-	render() {
-		return (
-			<motion.div className="resume" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
-				<iframe src="/images/EnidaVezelaj.pdf" height="70%" width="70%" frameBorder=".3" allowFullScreen />
-			</motion.div>
-		);
-	}
+  render() {
+    return (
+      <motion.div
+        className="resume"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
+        <iframe
+          src="/images/Enida_00.pdf"
+          height="70%"
+          width="70%"
+          frameBorder=".3"
+          allowFullScreen
+        />
+      </motion.div>
+    );
+  }
 }
 export default Resume;
